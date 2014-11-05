be86fb04d868d42675e9fcf85e7d9f76f102c2a8
brandon <josueknd@hotmail.es>
2014-11-05 11:56:25 -0600

    version 2

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3,6 +3,17 @@
  * Copyright 2011-2014 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
+$(document).ready(function(){
+  $("div").hover(
+    function(){
+    $(this).addClass("active");
+    },
+    function(){
+    $(this).removeClass("active");    
+    }
+  );
+
+});
 
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
---
+++ js/script.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    $("div").fadeIn("slow");
+    $("div").fadeTo("fast",0.8);
+    $("img").fadeTo("fast",0.8);
+    $("#hhh").mouseenter(function() {
+        $("#hhh").fadeTo("fast",1);
+        });
+    $("#hhh").mouseleave(function(){
+        $("#hhh").fadeTo("fast",0.5)
+        
+        })
+    $("#hhh2").mouseenter(function() {
+    $("#hhh2").fadeTo("fast",1);
+        });
+    $("#hhh2").mouseleave(function(){
+        $("#hhh2").fadeTo("fast",0.5)
+        
+        })
+    $("#hhh3").mouseenter(function() {
+    $("#hhh3").fadeTo("fast",1);
+        });
+    $("#hhh3").mouseleave(function(){
+        $("#hhh3").fadeTo("fast",0.5)
+        
+        })
+    $("#hhh4").mouseenter(function() {
+    $("#hhh4").fadeTo("fast",1);
+        });
+    $("#hhh4").mouseleave(function(){
+        $("#hhh4").fadeTo("fast",0.5)
+        
+        })
+    $("#hhh5").mouseenter(function() {
+    $("#hhh5").fadeTo("fast",1);
+        });
+    $("#hhh5").mouseleave(function(){
+        $("#hhh5").fadeTo("fast",0.5)
+        
+        })
+    $("#hhh6").mouseenter(function() {
+    $("#hhh6").fadeTo("fast",1);
+        });
+    $("#hhh6").mouseleave(function(){
+        $("#hhh6").fadeTo("fast",0.5)
+        
+        })
+    $("#hhh7").mouseenter(function() {
+    $("#hhh7").fadeTo("fast",1);
+        });
+    $("#hhh7").mouseleave(function(){
+        $("#hhh7").fadeTo("fast",0.5)
+        
+        })
+
+    $("img").mouseenter(function() {
+    $("img").fadeTo("fast",1);
+        });
+    $("img").mouseleave(function(){
+        $("img").fadeTo("fast",0.5)
+        
+        })
+
+   $('#name').mouseenter(function() {
+       $(this).animate({
+           height: '+=10px'
+       });
+   });
+   $('#name').mouseleave(function() {
+       $(this).animate({
+           height: '-=10px'
+       }); 
+   });
+  $("#name").click(function(){
+window.open("https://www.facebook.com/alf.uchiha.note.kira")
+  });
+
+    });
