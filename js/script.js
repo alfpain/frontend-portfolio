95ad5ee3801943b31a3f2487ff2f038d33517895
brandon <josueknd@hotmail.es>
2014-11-06 12:09:29 -0600

    version 3

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("div").fadeIn("slow");
     $("div").fadeTo("fast",0.8);
     $("img").fadeTo("fast",0.8);
+    $("p").fadeTo("fast",0.8);
     $("#hhh").mouseenter(function() {
         $("#hhh").fadeTo("fast",1);
         });
@@ -73,5 +74,60 @@ $(document).ready(function(){
   $("#name").click(function(){
 window.open("https://www.facebook.com/alf.uchiha.note.kira")
   });
+ $('#hhh').mouseenter(function() {
+    $("#hhh").addClass('one');
 
+    });
+     $("#hhh").mouseleave(function(){
+        $("#hhh").toggleClass("one")
+        
+        })
+ $('#hhh2').mouseenter(function() {
+    $("#hhh2").addClass('one');
+
+    });
+     $("#hhh2").mouseleave(function(){
+        $("#hhh2").toggleClass("one")
+        
+        })
+ $('#hhh3').mouseenter(function() {
+    $("#hhh3").addClass('one');
+
+    });
+     $("#hhh3").mouseleave(function(){
+        $("#hhh3").toggleClass("one")
+        
+        })
+ $('#hhh4').mouseenter(function() {
+    $("#hhh4").addClass('one');
+
+    });
+     $("#hhh4").mouseleave(function(){
+        $("#hhh4").toggleClass("one")
+        
+        })
+ $('#hhh5').mouseenter(function() {
+    $("#hhh5").addClass('one');
+
+    });
+     $("#hhh5").mouseleave(function(){
+        $("#hhh5").toggleClass("one")
+        
+        })
+ $('#hhh6').mouseenter(function() {
+    $("#hhh6").addClass('one');
+
+    });
+     $("#hhh6").mouseleave(function(){
+        $("#hhh6").toggleClass("one")
+        
+        })
+ $('#hhh7').mouseenter(function() {
+    $("#hhh7").addClass('one');
+
+    });
+     $("#hhh7").mouseleave(function(){
+        $("#hhh7").toggleClass("one")
+        
+        })
     });
